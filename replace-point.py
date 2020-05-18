8434861ee6757f5b5289a62d4d6068f4785e81a5
Pål Jakobsen <paaljakobsen1@gmail.com>
2020-05-18 22:29:34 +0200

    Update replace-point.py

--- replace-point.py
+++ replace-point.py
@@ -1,20 +1,19 @@
 street = "NESEVEIEN"
-point = "LLLLLLL000"
-point = "PUNKTID000"
+point = "PUNKTID"
 
 def readfile():  # put domains in file in to list called 'domains'
     domains = []
     newfile = []
     namecount = 0
     file = open('newfile.txt', 'w')
-    #file = open('newfile.txt', 'a')
 
     with open('neseveien.kof', 'r') as infile:
         for line in infile:
-            line = line.strip()
-            digg = point+str(namecount)
-            x = line.replace(street, digg)
             namecount += 1
+            he = format(namecount, '03')
+            line = line.strip()
+            digg = point+he
+            x = line.replace(street, digg)
             print(x)
             newfile.append(' ' + x + '\n')
             file.write(' ' + x + '\n')
